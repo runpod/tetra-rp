096239fae42dbcfafb141b48f294942e0e840990
Dean Quiñanola <dean.quinanola@runpod.io>
2025-03-31 11:16:29 -0700

    feat: resource utility, `inquire` to map runpod calls to pydantic models

--- tetra/core/resources/__init__.py
+++ tetra/core/resources/__init__.py
@@ -4,12 +4,14 @@ from .base import DeployableResource
 from .gpu import GpuType, GpuTypeDetail, GpuGroups
 from .serverless import ServerlessResource, ServerlessResourceInput
 from .template import TemplateResource
+from .utils import inquire
 
 
 runpod.api_key = os.getenv("RUNPOD_API_KEY")
 
 
 __all__ = [
+    "inquire",
     "runpod",
     "DeployableResource",
     "GpuType",
