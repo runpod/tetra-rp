b194351d7a4f131cfb49d89d1b8a2541fc8a81bf
Dean Quiñanola <dean.quinanola@runpod.io>
2025-04-16 10:39:14 -0700

    cleanup: removed unused files

--- tetra/__init__.py
+++ tetra/__init__.py
@@ -2,15 +2,12 @@ import os
 from .logger import setup_logging
 setup_logging(os.environ.get("LOG_LEVEL", "INFO"))
 
-from . import remote_execution_pb2, remote_execution_pb2_grpc
 from .core.resources.resource_manager import ResourceManager
 from .core.resources.serverless import ServerlessResource
 from .client import remote
 
 __all__ = [
     "remote",
-    "remote_execution_pb2",
-    "remote_execution_pb2_grpc",
     "ResourceManager",
     "ServerlessResource",
 ]
