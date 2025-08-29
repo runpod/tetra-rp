6251259f8321a55e36d0b744383c6525417bf3e6
Dean Quiñanola <dean.quinanola@runpod.io>
2025-08-28 20:43:58 -0700

    refactor: proper annotations for IDE readability on CPU and GPU enums

--- src/tetra_rp/core/resources/cpu.py
+++ src/tetra_rp/core/resources/cpu.py
@@ -16,19 +16,43 @@ class CpuInstanceType(str, Enum):
     """
 
     # 3rd Generation General Purpose (RAM multiplier: 4.0)
-    CPU3G_1_4 = "cpu3g-1-4"  # 1 vCPU, 4GB RAM
-    CPU3G_2_8 = "cpu3g-2-8"  # 2 vCPU, 8GB RAM
-    CPU3G_4_16 = "cpu3g-4-16"  # 4 vCPU, 16GB RAM
-    CPU3G_8_32 = "cpu3g-8-32"  # 8 vCPU, 32GB RAM
+
+    CPU3G_1_4 = "cpu3g-1-4"
+    """1 vCPU, 4GB RAM, max 10GB container disk"""
+
+    CPU3G_2_8 = "cpu3g-2-8"
+    """2 vCPU, 8GB RAM, max 20GB container disk"""
+
+    CPU3G_4_16 = "cpu3g-4-16"
+    """4 vCPU, 16GB RAM, max 40GB container disk"""
+
+    CPU3G_8_32 = "cpu3g-8-32"
+    """8 vCPU, 32GB RAM, max 80GB container disk"""
 
     # 3rd Generation Compute-Optimized (RAM multiplier: 2.0)
-    CPU3C_1_2 = "cpu3c-1-2"  # 1 vCPU, 2GB RAM
-    CPU3C_2_4 = "cpu3c-2-4"  # 2 vCPU, 4GB RAM
-    CPU3C_4_8 = "cpu3c-4-8"  # 4 vCPU, 8GB RAM
-    CPU3C_8_16 = "cpu3c-8-16"  # 8 vCPU, 16GB RAM
+
+    CPU3C_1_2 = "cpu3c-1-2"
+    """1 vCPU, 2GB RAM, max 10GB container disk"""
+
+    CPU3C_2_4 = "cpu3c-2-4"
+    """2 vCPU, 4GB RAM, max 20GB container disk"""
+
+    CPU3C_4_8 = "cpu3c-4-8"
+    """4 vCPU, 8GB RAM, max 40GB container disk"""
+
+    CPU3C_8_16 = "cpu3c-8-16"
+    """8 vCPU, 16GB RAM, max 80GB container disk"""
 
     # 5th Generation Compute-Optimized (RAM multiplier: 2.0)
-    CPU5C_1_2 = "cpu5c-1-2"  # 1 vCPU, 2GB RAM
-    CPU5C_2_4 = "cpu5c-2-4"  # 2 vCPU, 4GB RAM
-    CPU5C_4_8 = "cpu5c-4-8"  # 4 vCPU, 8GB RAM
-    CPU5C_8_16 = "cpu5c-8-16"  # 8 vCPU, 16GB RAM
+
+    CPU5C_1_2 = "cpu5c-1-2"
+    """1 vCPU, 2GB RAM, max 15GB container disk"""
+
+    CPU5C_2_4 = "cpu5c-2-4"
+    """2 vCPU, 4GB RAM, max 30GB container disk"""
+
+    CPU5C_4_8 = "cpu5c-4-8"
+    """4 vCPU, 8GB RAM, max 60GB container disk"""
+
+    CPU5C_8_16 = "cpu5c-8-16"
+    """8 vCPU, 16GB RAM, max 120GB container disk"""
